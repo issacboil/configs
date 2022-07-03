4f20c33341d6807c2ce717614ca6b4c626784460
Issacboi <40278713+issacboil@users.noreply.github.com>
2022-07-03 17:08:49 -0300

    started working on install-programs script

---
+++ install-programs.sh
@@ -0,0 +1,10 @@
+# installs all the programs required for my configs 
+
+# exits if command fails 
+set -e 
+
+# cd into the script file location 
+cd "$(dirname "$0")"
+
+echo ""
+echo "install i3-gaps rofi thunar ly polybar terminator yay xorg i3-lock"
